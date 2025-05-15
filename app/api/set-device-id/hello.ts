4e2d1526689a26b66f4accbb5a5088624b734cad
Peyman Ahmadi <peymangeek@gmail.com>
2025-05-16 01:44:31 +0330

    Deleted wrong api route

--- app/api/set-device-id/hello.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-
-export default function handler(req: NextRequest, res: NextResponse) {
-    console.log(req)
-    return NextResponse.json({
-        message: "Test :)"
-    })
-}
