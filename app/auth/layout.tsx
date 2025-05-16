f785f85491a4e45dcacbbfcba490b314edb72125
Peyman Ahmadi <peymangeek@gmail.com>
2025-05-16 21:19:57 +0330

    Auth overflow fixed

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,10 +6,10 @@ import SignInLink from '../components/pages/auth/common/sign-in-link'
 
 const layout = ({ children }: { children: ReactNode }) => {
     return (
-        <section>
+        <section className='!overflow-x-hidden'>
             <BackButton />
             <AuthShapesSVG className='absolute -top-[68px] -right-[68px]' />
-            <main className='relative flex-col gap-[43px] min-w-dvw min-h-dvh flex items-center justify-center'>
+            <main className='relative flex-col gap-[43px] min-w-dvw min-h-dvh !overflow-x-hidden flex items-center justify-center'>
                 <AuthLogo />
                 {children}
                 <SignInLink />
--- app/core/types.ts
+++ app/core/types.ts
@@ -31,3 +31,103 @@ export type OTPValidationType = {
 }
 
 export type LoginMethodType = "OTP" | "PASSWORD"
+
+
+
+export interface HolyObjectType {
+  value?: string;
+  isInformation?: boolean;
+  id?: number;
+  document?: string;
+  wordByWord?: boolean;
+  trans?: string;
+  bookIndex?: number;
+  seasonName?: string;
+  sentenceSeen?: null;
+  sentenceArchive?: null;
+  word?: any[];
+  index?: number;
+  idsentence?: number;
+  idsentenceTranslates?: number;
+  wordTranslates?: null;
+  userSentences?: null;
+  documentSentences?: any[];
+  information_sentences?: InformationSentence[];
+  sentence?: Sentence;
+  audioSentenceDTO?: AudioSentenceDTO[];
+  audio?: Audio[];
+  translate?: null;
+  endPage?: number;
+  quranVerse?: QuranVerse; // اضافه کردن آیه قرآن
+}
+
+interface InformationSentence {
+  id: number;
+  idsentence: number;
+  value: string;
+  information: Information;
+  idInformation?: number;
+}
+
+interface Information {
+  id?: number;
+  value?: string;
+}
+
+interface Sentence {
+  id: number;
+  idseason: number;
+  index: number;
+  document: string;
+  wordByWord: boolean;
+  trans: string;
+  bookIndex: number;
+  seasonName: string;
+  sentenceFavorite?: null;
+  sentenceSeen?: null;
+  sentenceArchive?: null;
+  words?: Word[];
+  value: string;
+  wordTranslates?: null;
+  userSentences?: null;
+  documentSentences?: null;
+  sentenceTranslates?: any[];
+}
+
+interface Word {
+  id: number;
+  idsentence: number;
+  index: number;
+  value: string;
+}
+
+interface AudioSentenceDTO {
+  id: number;
+  idAudioSentence?: number;
+  idSeason?: number;
+  startPage?: number;
+  endPage?: number;
+  duration?: number;
+  audioWords?: any[];
+  idWord?: number;
+}
+
+interface Audio {
+  id: number;
+  idBook: number;
+  idSeason: number;
+  idmedia: number;
+  totalTime: number;
+  audioSentences?: null;
+}
+
+interface QuranVerse {
+  id: number;
+  surah: number;
+  verseNumber: number;
+  text: string;
+  translation: string;
+  transliteration: string;
+  audioUrl: string;
+  tafsir: string;
+}
