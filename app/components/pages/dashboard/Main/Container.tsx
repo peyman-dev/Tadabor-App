b7557e8b56aa4324a00dd0997e315ad2074d6dad
Peyman Ahmadi <peymangeek@gmail.com>
2025-05-19 21:13:25 +0330

    .

--- app/components/pages/dashboard/Main/Container.tsx
+++ app/components/pages/dashboard/Main/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerInterface extends AllHTMLAttributes<HTMLDivElement> {
 const Container = ({ children }: { children: ReactNode }) => {
     return (
         <main className='container w-full py-10 shadow-lg rounded-[50px]'>
-            <section id="content" className='w-full container md:p-5 flex items-center lg:items-start  justify-center lg:flex-row-reverse !flex-col  gap-11 mx-auto'>
+            <section id="content" className='w-full container md:p-5 flex items-center lg:items-start  justify-center lg:flex-row-reverse flex-col gap-11 mx-auto'>
                 {children}
             </section>
         </main>
