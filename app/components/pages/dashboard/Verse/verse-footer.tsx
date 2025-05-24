0e3aa281e44393ab4a64a0e61fe000e45312c758
Peyman Ahmadi <peymangeek@gmail.com>
2025-05-24 10:53:37 +0330

    Push for deploying on vercel

--- app/components/pages/dashboard/Verse/verse-footer.tsx
+++ app/components/pages/dashboard/Verse/verse-footer.tsx
@@ -6,20 +6,26 @@ import { InformationSentence } from "@/app/core/types/types";
 import React from "react";
 
 const VerseFooter = ({ content }: { content?: any }) => {
-    const { data } = useHolyStore()
+    const { data } = useHolyStore();
     const informationSentences = (data?.informationSentences as InformationSentence[]) || [];
 
     const juzItem = useQuickInformation(informationSentences, "جزء");
     const hezbItem = useQuickInformation(informationSentences, "حزب");
     const siaqItem = useQuickInformation(informationSentences, "سیاق");
 
-
     return (
         <>
             <FooterHolder className="absolute !left-0 -bottom-6 !right-0 !mx-auto" />
             <div className="absolute flex flex-col items-center justify-center !left-0 -bottom-1.5 sm:gap-1 !right-0 !mx-auto">
                 <p className="text-xs sm:text-xs">
-                 جزء {juzItem?.value || "نامشخص"} | حزب {hezbItem?.value || "نامشخص"} | سیاق {siaqItem?.value || "نامشخص"}
+                    {/* فقط در صورت وجود مقدار، آیتم نمایش داده می‌شود */}
+                    {juzItem?.value && `جزء ${juzItem.value}`}
+                    {juzItem?.value && (hezbItem?.value || siaqItem?.value) && " | "}
+                    {hezbItem?.value && `حزب ${hezbItem.value}`}
+                    {hezbItem?.value && siaqItem?.value && " | "}
+                    {siaqItem?.value && `سیاق ${siaqItem.value}`}
+                    {/* اگر هیچ آیتمی وجود نداشت، متن "نامشخص" نمایش داده می‌شود */}
+                    {!juzItem?.value && !hezbItem?.value && !siaqItem?.value && "نامشخص"}
                 </p>
                 <p className="text-[10px] sm:text-xs">
                     گروه تحقیقاتی تدبرّ درکلام وحی
@@ -28,6 +34,5 @@ const VerseFooter = ({ content }: { content?: any }) => {
         </>
     );
 };
-
 
 export default VerseFooter;
